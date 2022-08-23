f4f0f959aa4de4c2b4cfcc7bfbddb31879390c8a
hagverdi11 <hagverdiam@code.edu.az>
2022-08-24 03:23:28 +0400

    Added homeworl

---
+++ Homework 4 String - Array/assets/js/script.js
@@ -0,0 +1,68 @@
+ let country = "Azerbaycan, Baku, Shemkir, Baku"
+ let city = "Baku"
+
+
+// console.log(country.includes("e"));
+
+// console.log(country.match(/e/g));
+
+// let proccess = country.indexOf("Baku", 25);
+// console.log(proccess);
+
+// let result = country.substring(0,proccess)
+// console.log(result);
+
+//  console.log(country.indexOf("Baku", 10));
+// console.log(country.length);
+
+
+// console.log(country.toString);
+
+
+// console.log(country.toLocaleLowerCase);
+
+// console.log(country.toLocaleUpperCase);
+
+
+// console.log(country.slice(-8,-2));
+
+
+//console.log(country.substring(3,5));
+
+
+// console.log(country.substr(2,3));
+
+
+// console.log(country.replace("z","l"));
+
+
+
+// console.log(country.concat(city));
+
+// console.log(country.trim());
+
+
+
+//console.log(country.startsWith("Azez"));
+
+//console.log(country.charAt(9));
+
+// console.log(country.charCodeAt(9));
+
+
+// console.log(country.split(" "));
+
+
+// console.log(country.indexOf("Baku"));
+
+// function Search(str) {
+//     if (country.indexOf == -1) {
+//         console.log("Not found.");
+//         return
+//     }
+//     else console.log("found");
+// }
+
+// Search("Baku")
+
+
